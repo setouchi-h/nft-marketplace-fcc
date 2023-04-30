3d070ea50fce6fd48af9acd2cab14d7bfe09026a
setouchi-h <setouchi.kazuki@gmail.com>
2023-05-01 00:53:55 +0900

    sleepy

--- hardhat-nft-marketplace-fcc/scripts/mint.js
+++ hardhat-nft-marketplace-fcc/scripts/mint.js
@@ -1,4 +1,4 @@
-const { ethers, network } = require("hardhat")
+const { ethers } = require("hardhat")
 const { moveBlocks } = require("../utils/move-blocks")
 
 const PRICE = ethers.utils.parseEther("0.1")
@@ -9,15 +9,6 @@ async function mintAndList() {
     console.log("Minting...")
     const mintTx = await basicNft.mintNft()
     const mintTxReceipt = await mintTx.wait(1)
-    const tokenId = mintTxReceipt.events[0].args.tokenId
-
-    console.log("Approving NFT...")
-    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
-    await approvalTx.wait(1)
-    console.log("Listing NFT...")
-    const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
-    await tx.wait(1)
-    console.log("Listed!")
 
     if (network.config.chainId == 31337) {
         // Moralis has a hard time if you move more than 1 at once!
