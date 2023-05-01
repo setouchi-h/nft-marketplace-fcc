d731fdbe4d5e0892366386d90358a5b0c67acba3
setouchi-h <setouchi.kazuki@gmail.com>
2023-05-01 13:38:44 +0900

    update sell nft

--- nextjs-nft-marketplace-fcc/README.md
+++ nextjs-nft-marketplace-fcc/README.md
@@ -4,6 +4,7 @@
         2. If not, you can buy the listing
 2. Sell Page:
     1. You can list your NFT on the marketplace
+    2. and withdraw proceeds
 
 
 For reading the events, we will
--- nextjs-nft-marketplace-fcc/src/pages/sell-nft.jsx
+++ nextjs-nft-marketplace-fcc/src/pages/sell-nft.jsx
@@ -1,5 +1,99 @@
 import Image from "next/image"
+import { ethers } from "ethers"
+import { Form, useNotification } from "web3uikit"
+import nftAbi from "../../constants/BasicNft.json"
+import nftMarketplaceAbi from "../../constants/NftMarketplace.json"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import networkMapping from "../../constants/networkMapping.json"
 
 export default function Home() {
-    return <>main</>
+    const { chainId, account, isWeb3Enabled } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
+
+    const dispatch = useNotification()
+    const { runContractFunction } = useWeb3Contract()
+
+    async function approveAndList(data) {
+        console.log("Approving...")
+        const nftAddress = data.data[0].inputResult
+        const tokenId = data.data[1].inputResult
+        const price = ethers.utils.parseUnits(data.data[2].inputResult, "ether").toString()
+
+        const approveOptions = {
+            abi: nftAbi,
+            contractAddress: nftAddress,
+            functionName: "approve",
+            params: {
+                to: marketplaceAddress,
+                tokenId,
+            },
+        }
+
+        await runContractFunction({
+            params: approveOptions,
+            onSuccess: handleApproveSuccess(nftAddress, tokenId, price),
+            onError: (error) => console.log(error),
+        })
+    }
+
+    async function handleApproveSuccess(nftAddress, tokenId, price) {
+        console.log("OK! now time to list")
+        const listOptions = {
+            abi: nftMarketplaceAbi,
+            contractAddress: marketplaceAddress,
+            functionName: "listItem",
+            params: {
+                nftAddress,
+                tokenId,
+                price,
+            },
+        }
+
+        await runContractFunction({
+            params: listOptions,
+            onSuccess: () => handleListSuccess(),
+            onError: (error) => console.log(error),
+        })
+    }
+
+    async function handleListSuccess() {
+        dispatch({
+            type: "success",
+            message: "NFT listing",
+            title: "NFT listed",
+            position: "topR",
+        })
+    }
+
+    return (
+        <div>
+            <Form
+                onSubmit={approveAndList}
+                data={[
+                    {
+                        name: "NFT Address",
+                        type: "text",
+                        inputWidth: "50%",
+                        value: "",
+                        key: "nftAddress",
+                    },
+                    {
+                        name: "Token ID",
+                        type: "number",
+                        value: "",
+                        key: "tokenId",
+                    },
+                    {
+                        name: "Price (in ETH)",
+                        type: "number",
+                        value: "",
+                        key: "price",
+                    },
+                ]}
+                title="Sell your NFT!"
+                id="Main Form"
+            />
+        </div>
+    )
 }
